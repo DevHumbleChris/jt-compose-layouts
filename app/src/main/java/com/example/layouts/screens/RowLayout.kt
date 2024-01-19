a5dd5f6654b4899060ad62cfc8e02d2db2b20919
DevHumbleChris <dev.humblechris254@gmail.com>
2024-01-19 18:41:09 +0300

    added the Row and Scaffold Layout

--- app/src/main/java/com/example/layouts/screens/RowLayout.kt
+++ app/src/main/java/com/example/layouts/screens/RowLayout.kt
@@ -1,9 +1,43 @@
 package com.example.layouts.screens
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RowLayout(navController: NavHostController) {
-
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { 
+                    Text(text = "Hello Row Layout")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        navController.navigate("home")
+                    }) {
+                        Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { it -> 
+        Row(
+            modifier = Modifier.padding(it)
+        ) {
+            Text(text = "Text One")
+            Text(text = "Text Two")
+        }
+    }
 }
--- app/src/main/java/com/example/layouts/screens/ScaffoldLayout.kt
+++ app/src/main/java/com/example/layouts/screens/ScaffoldLayout.kt
@@ -1,9 +1,57 @@
 package com.example.layouts.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScaffoldLayout(navController: NavHostController) {
-
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text(text = "Hello Scaffold layout")
+            },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        navController.navigate("home")
+                    }) {
+                        Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+            }
+        },
+        bottomBar = {
+            BottomAppBar {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Filled.Home, contentDescription = null)
+                }
+            }
+        }
+    ) { it ->
+        Column(
+            modifier = Modifier.padding(it)
+        ) {
+            Text(text = "In Scaffold Example")
+        }
+    }
 }
